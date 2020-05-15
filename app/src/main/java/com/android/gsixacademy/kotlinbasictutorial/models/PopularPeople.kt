186bb56aa4f911986c137ba1ab4487741253bff6
Velkovska89 <velkovska89@gmail.com>
2020-05-15 21:34:05 +0200

    Search People Api Implemented

---
+++ app/src/main/java/com/android/gsixacademy/kotlinbasictutorial/models/PopularPeople.kt
@@ -0,0 +1,10 @@
+package com.android.gsixacademy.kotlinbasictutorial.models
+
+class PopularPeople (
+
+
+    val page: Int?,
+    val total_results: Int?,
+    val total_pages: Int?,
+    val results: ArrayList<PersonResult>
+)
